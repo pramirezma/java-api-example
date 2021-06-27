ec731993c60ec39eb2c56be521b00c9e16a6418c
Pablo Ramírez <pablo.ram@gmail.com>
2021-06-26 20:00:25 -0400

    first commit

---
+++ src/test/java/org/pabloram/apitest/ApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pabloram.apitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
