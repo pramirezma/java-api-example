009aec2d40c1fb472395282caf7acca5c115c98c
Pablo Ramírez <pablo.ram@gmail.com>
2021-07-04 21:14:05 -0400

    added to paths

--- src/main/java/org/pabloram/apitest/GreetingController.java
+++ src/main/java/org/pabloram/apitest/GreetingController.java
@@ -15,4 +15,8 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+    @GetMapping("/hello")
+    public Greeting hello(@RequestParam(value = "name", defaultValue = "Mundo") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 }
