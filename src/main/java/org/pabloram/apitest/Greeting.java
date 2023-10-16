5638fc1f524f08ade52d7c73bfbfe22222ec6e28
Pablo Ramirez <pablo_a_ramirez@mckinsey.com>
2023-10-16 20:28:25 -0300

    feat: added logging

--- src/main/java/org/pabloram/apitest/Greeting.java
+++ src/main/java/org/pabloram/apitest/Greeting.java
@@ -1,13 +1,18 @@
 package org.pabloram.apitest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Greeting {
 
+    private static final Logger logger = LogManager.getLogger("HelloWorld");
     private final long id;
     private final String content;
 
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
+        logger.info("id:"+ id + " " + "content:" + content);
     }
 
     public long getId() {
